a50445e8adbbdf07a5f0b8f56f376175cd1890f0
Jesper Østergaard Jensen <jesper@onlinelife.dk>
2023-02-19 14:32:25 +0100

    Adjusted HelloWorldInformationBlock

--- src/InformationBlocks/HelloWorldInformationBlock.php
+++ src/InformationBlocks/HelloWorldInformationBlock.php
@@ -6,4 +6,14 @@ use CodeHqDk\RepositoryInformation\Model\BaseInformationBlock;
 
 class HelloWorldInformationBlock extends BaseInformationBlock
 {
+    public static function fromArray(array $array): self {
+        return new self(
+            $array['headline'],
+            $array['label'],
+            $array['value'],
+            $array['modified_timestamp'],
+            $array['details'],
+            $array['information_origin'],
+       );
+    }
 }
